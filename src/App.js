02a0bb912bdf257fbd26a79d35357e3fe1d80acc
Yinglao Liu <yinglao@meta.com>
2023-07-08 21:30:23 -0700

    test change

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
+import { Component } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component {
+  render() {
+    return <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to.
         </p>
         <a
           className="App-link"
@@ -19,7 +21,7 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  }
 }
 
 export default App;
